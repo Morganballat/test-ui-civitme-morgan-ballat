3168eef3ae8a91dadfbdb1d3f85681c58be476fc
Morgan Ballat <66262064+Morganballat@users.noreply.github.com>
2021-10-12 15:35:32 +0200

    css mobile done

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -21,60 +21,62 @@ const Profile = () => (
 	<div className='profile'>
 		<div className='profile__window'>
 			<h1 className='profile__window_title'>Profil</h1>
-			<p className=' profile__window_question'>
+			<p className='profile__window_question'>
 				Lequel de ces deux avatars préférez-vous ?
 			</p>
 			<div className='profile__window_avatars'>
-				<img className='profile__window_avatar_man' src={Man} alt='man' />
-				<img className='profile__window_avatar_woman' src={Woman} alt='woman' />
+				<img className='profile__window_avatar man' src={Man} alt='man' />
+				<img className='profile__window_avatar woman' src={Woman} alt='woman' />
 			</div>
 			<div className='profile__window_colors'>
 				<div className='profile__window_colors_skin'>
-					<img className='profile__window_skin_color' src={Skin} alt='skin' />{' '}
+					<img className='profile__window_skin' src={Skin} alt='skin' />
+
 					<img
-						className='profile__window_character man'
+						className='profile__window_skin_color beige'
 						src={LightBeige}
 						alt='man'
-					/>{' '}
+					/>
 					<img
-						className='profile__window_skin_color_pink'
+						className='profile__window_skin_color pink'
 						src={Pink}
 						alt='Pink'
 					/>
 					<img
-						className='profile__window_skin_color_beige'
+						className='profile__window_skin_color beige'
 						src={Beige}
 						alt='man'
 					/>
 					<img
-						className='profile__window_skin_color_brown'
+						className='profile__window_skin_color brown'
 						src={Brown}
 						alt='brown'
 					/>
 				</div>
-				<div className='profile__window_tshirt_color'>
+				<div className='profile__window_colors_tshirt'>
 					<img className='profile__window_tshirt' src={Tshirt} alt='t-shirt' />
 					<img
-						className='tshirt__window_tshirt_color_gray'
+						className='profile__window_tshirt_color gray'
 						src={Gray}
 						alt='gray'
 					/>{' '}
 					<img
-						className='tshirt__window_tshirt_color_green'
+						className='profile__window_tshirt_color green'
 						src={Green}
 						alt='green'
 					/>
 					<img
-						className='profile__window_tshirt_color_red'
+						className='profile__window_tshirt_color red'
 						src={Red}
 						alt='red'
 					/>
 					<img
-						className='profile__window_tshirt_color_striped'
+						className='profile__window_tshirt_color striped'
 						src={Striped}
 						alt='white_and_blue'
 					/>
 				</div>
+				<button className='profile__window_color_btn'>valider</button>
 			</div>
 		</div>
 	</div>
